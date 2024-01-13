c060593033fef7c40d61e6e02a7c2f2d9c3391e6
dengweibin <gdoudeng@qq.com>
2024-01-13 22:44:48 +0800

    feat: 使用新的网络工具

--- src/utils/http/index.ts
+++ src/utils/http/index.ts
@@ -1,115 +1,90 @@
-import { IBaseResponse } from "@src/utils/http/types";
-import type { MiddleWare, RequestOptions } from "@cyzeal/fetch-onion-model";
-import { create } from "@cyzeal/fetch-onion-model";
-import timeout from "@cyzeal/fetch-onion-model/lib/middleware/timeout.js";
+import { CFetch, interceptors } from "./fetch";
 import { userDao } from "@src/dao/UserDao";
-import { Alert } from "react-native";
-import { reset } from "@src/navigation/RootNavigation";
+import { IBaseResponse } from "@src/utils/http/types";
 
-const defaultConfig: RequestOptions = {
-  baseUrl: __DEV__ ? "https://recite.dev.tcpsapp.com/admin" : "https://recite.tcpsapp.com/admin",
+const defaultConfig = {
+  baseURL: __DEV__ ? "https://recite.dev.tcpsapp.com/admin" : "https://recite.tcpsapp.com/admin",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json"
   }
 };
 
-const reportMiddleware: MiddleWare = async (ctx, next) => {
-  console.log(`${ctx.method} ${ctx.url}`);
-  await next();
-  console.log(`${ctx.method} ${ctx.url} done. status: ${ctx.status}`);
+const ErrorMsgMap = {
+  3: "没有操作权限，请重新登录！",
+  8: "当前账号在其他地方登陆，请重新登陆！",
+  7: "登陆失效，请重新登陆！",
+  105: "该用户未在平台上注册，请重新登陆！"
 };
 
-const interceptorsRequestMiddleware: MiddleWare = async (ctx, next) => {
-  if (userDao.token) {
-    ctx.options.headers = {
-      "Content-Type": "application/json",
-      "X-FM-TOKEN": userDao.token
-    };
-  } else {
-    ctx.options.headers = {
-      "Content-Type": "application/json"
-    };
+// 添加请求拦截器
+interceptors.request.use(config => {
+  // 这里是我项目使用到的js-cookie库，主要是为了拿到token，你们这里改成你们获取token的方式即可
+  const token = userDao.token;
+  config.headers = defaultConfig.headers;
+  if (token) {
+    config.headers["X-FM-Token"] = token;
   }
-};
+  return config;
+});
 
-const interceptorsResponseMiddleware: MiddleWare = async (ctx, next) => {
-  await next();
-
-};
-
-class Http {
-  private static instance = create(defaultConfig, [reportMiddleware, interceptorsRequestMiddleware, timeout(10000), interceptorsResponseMiddleware]);
-
-  request<T = any>(url: string, requestOptions: RequestOptions<IBaseResponse<T>>): Promise<IBaseResponse<T>> {
-    return new Promise((resolve, reject) => {
-      Http.instance<IBaseResponse<T>>(url, requestOptions)
-        .then((res) => {
-          this.handleResponse<T>(url, res, resolve, reject);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
+// 添加响应拦截器
+interceptors.response.use(
+  response => {
+    if (response && response.code !== 1) {
+      const errorMsg =
+        response && response.data && response.data.errors
+          ? response.data.errors.join(",")
+          : response.msg;
+      return Promise.reject(new Error(errorMsg || "Error"));
+    }
+    if (response) {
+      return response;
+    }
+    return Promise.reject("服务器繁忙，请稍后重试");
+  },
+  error => {
+    // 所有的响应异常 区分来源为取消请求/非取消请求
+    console.log(error); // for debug
+    return Promise.reject(error);
   }
+);
 
-  private handleResponse<T = any>(url: string, response: IBaseResponse, resolve: (value: IBaseResponse<T> | PromiseLike<IBaseResponse<T>>) => void, reject: (reason?: any) => void) {
-    // response.code：是与服务器端约定code：1表示请求成功，非1表示请求失败，msg：请求失败内容
-    if (response && response.code === 1) {
-      // 设置token
-      if (response && response.data && response.data.token) {
-        userDao.token = response.data.token;
+/**
+ * GET 请求时，拼接请求URL
+ * @param url 请求URL
+ * @returns {*}
+ */
+const handleUrl = (url: string): any => (params: Record<string, any>) => {
+  if (params) {
+    let paramsArray: Array<string> = [];
+    Object.keys(params).forEach(key => {
+      if (params[key] != null || params[key] != undefined) {
+        paramsArray.push(key + "=" + encodeURIComponent(params[key]));
       }
-      resolve(response);
-    } else if (response && response.code === 3 && response.msg && response.msg.includes("没有权限") && url !== "/security/logout") {
-      Alert.alert(
-        "提示",
-        "登陆过期，请重新登陆 :)",
-        [
-          {
-            text: "确定", onPress: async () => {
-              userDao.clearUserToken();
-              reset("Login");
-            }
-          }
-        ],
-        { cancelable: false }
-      );
-      reject(response.data && response.data.errors ? response.data.errors.join(",") : response.msg);
-    } else if (response && [7, 8].includes(response.code) && url !== "/security/logout") {
-      Alert.alert(
-        "提示",
-        response.code === 7 ? "登陆失效，请重新登陆！" : "当前账号在其他地方登陆，请重新登陆！",
-        [
-          {
-            text: "确定", onPress: async () => {
-              userDao.clearUserToken();
-              reset("Login");
-            }
-          }
-        ],
-        { cancelable: false }
-      );
-      reject(response.code === 7 ? "登陆失效，请重新登陆！" : "当前账号在其他地方登陆，请重新登陆！");
+    });
+    if (url.search(/\?/) === -1) {
+      typeof (params) === "object" ? url += "?" + paramsArray.join("&") : url;
     } else {
-      reject(response.data && response.data.errors ? response.data.errors.join(",") : response.msg);
+      url += "&" + paramsArray.join("&");
     }
   }
+  return url;
+};
 
-  async get<T>(url: string, params?: Record<string, unknown>) {
-    return await this.request<T>(url, {
-      method: "GET",
-      params
-    });
-  }
-
-  async post<T>(url: string, data?: Record<string, unknown>) {
-    return await this.request<T>(url, {
-      method: "POST",
-      data
-    });
-  }
+// get请求方法使用封装
+export function get<T>(url: string, params = {}): Promise<IBaseResponse<T>> {
+  return CFetch<T>(handleUrl(defaultConfig.baseURL + url)(params), {
+    method: "GET"
+  });
 }
 
-export const http = new Http();
+// post请求方法使用封装
+export function post<T>(url: string, data = {}): Promise<IBaseResponse<T>> {
+  return CFetch<T>(defaultConfig.baseURL + url, {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
+}
+
 
