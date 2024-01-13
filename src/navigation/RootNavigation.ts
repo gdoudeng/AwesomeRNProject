eabee706359367eaa78d6b52e5658aef7f05e26d
dengweibin <gdoudeng@qq.com>
2024-01-13 18:04:21 +0800

    feat: 使用新的导航工具

---
+++ src/navigation/RootNavigation.ts
@@ -0,0 +1,58 @@
+import { createNavigationContainerRef, NavigationAction, StackActions } from "@react-navigation/native";
+import { RootStackParamList } from "@src/navigation/GlobalStack";
+import Toast from "react-native-simple-toast";
+
+export const navigationRef = createNavigationContainerRef<RootStackParamList>();
+
+export function navigate(name: keyof RootStackParamList, params?: any) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name, params);
+  } else {
+    // You can decide what to do if the app hasn't mounted
+    // You can ignore this, or add these actions to a queue you can call later
+    Toast.show("导航还未挂载", Toast.LONG);
+  }
+}
+
+export function push(name: string, params?: object | undefined) {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.push(name, params));
+  } else {
+    Toast.show("导航还未挂载", Toast.LONG);
+  }
+}
+
+export function reset(name: string) {
+  if (navigationRef.isReady()) {
+    navigationRef.reset({ index: 0, routes: [{ name }] });
+  } else {
+    Toast.show("导航还未挂载", Toast.LONG);
+  }
+}
+
+export function replace(name: string, params?: object | undefined) {
+  if (navigationRef.isReady()) {
+    navigationRef.dispatch(StackActions.replace(name, params));
+  } else {
+    Toast.show("导航还未挂载", Toast.LONG);
+  }
+}
+
+export function goBack() {
+  if (navigationRef.isReady()) {
+    navigationRef.goBack();
+  } else {
+    Toast.show("导航还未挂载", Toast.LONG);
+  }
+}
+
+export function dispatch(action: NavigationAction) {
+  if (navigationRef.isReady()) {
+    // Perform navigation if the app has mounted
+    navigationRef.dispatch(action);
+  } else {
+    // You can decide what to do if the app hasn't mounted
+    // You can ignore this, or add these actions to a queue you can call later
+    Toast.show("导航还未挂载", Toast.LONG);
+  }
+}
